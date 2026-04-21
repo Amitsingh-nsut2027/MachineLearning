a9011d98f77a6a13bc95a46d574472753a727578
Amitsingh-nsut2027 <as8840677@gmail.com>
2026-04-22 04:04:21 +0530

    Day 01 - Learned NumPy array operations

---
+++ 02_NumPy/day01_numpy_basics.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+arr = np.array([1,2,3,4,5])
+
+print("Array:", arr)
+print("Dimensions:", arr.ndim)
+print("Shape:", arr.shape)
+print("Datatype:", arr.dtype)
+
+print("First element:", arr[0])
+print("Last element:", arr[-1])
+
+print("Add 5:", arr + 5)
+print("Multiply by 2:", arr * 2)
